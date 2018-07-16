cddb73c06279cb3fba9d788497f1444e64490879
ayesha ansari <ayesha.x.ansari@gmail.com>
2018-07-16 18:47:30 +0000

    automatically backed up by learn

--- app/helpers/songs_helper.rb
+++ app/helpers/songs_helper.rb
@@ -2,7 +2,7 @@ module SongsHelper
     def display_artist(song)
       if song.artist
         link_to song.artist.name, song.artist
-        link_to 'Adddd Artist', edit_song_path(song)
+        link_to 'Adddd dddddddddArtist', edit_song_path(song)
       end
 
 
